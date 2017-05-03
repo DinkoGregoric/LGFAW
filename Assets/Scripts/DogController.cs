cd8706887392cdbc88b08e841761774ed72bf4cb
Dinko[cURL] <dinkochess@gmail.com>
2017-05-03 16:55:33 +0200

    Basic blocks of game

---
+++ Assets/Scripts/DogController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogController : MonoBehaviour {
+
+    private Rigidbody2D dogBody;
+    public float jumpForce = 500f;
+
+	// Use this for initialization
+	void Start () {
+        dogBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            dogBody.AddForce(transform.up * jumpForce);
+        }
+	}
+}
