8f6a4c0f903d6cc9aa33fb1cf7602562335ba34e
Dinko[cURL] <dinkochess@gmail.com>
2017-05-04 18:45:01 +0200

    First level is playable, only thing left is gradually decaying the healthbar over time

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -1,23 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MoveLeft : MonoBehaviour {
 
-    public float speed = 10f;
+    public HealthBarModifier hBModifier;
+    public float speed = 5f;
+    public float hungerTime = 10;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        hBModifier = GameObject.FindObjectOfType<HealthBarModifier>();
+        /*StartCoroutine("MakeHimHungry");*/
+    }
 	
 	// Update is called once per frame
 	void Update () {
         transform.position += Vector3.left * speed * Time.deltaTime;
+        
+        if (hBModifier.getSpriteIndex() == 4) {
+            SceneManager.LoadScene("Game");
+        }
+        
 
-	}
+    }
 
     private void OnTriggerEnter2D(Collider2D collision) {
         Destroy(gameObject);
+        
+        if(this.tag == "Poison") {
+            hBModifier.ChangeHBar(1);
+        }
+
+        if (hBModifier.getSpriteIndex() != 0) {
+            if (this.tag == "Consumable") {
+                hBModifier.ChangeHBar(-1);
+            }
+        }
+            
+
+        if (hBModifier.getSpriteIndex() == 4) {
+            SceneManager.LoadScene("Game");
+        }
     }
+
+   /* IEnumerator MakeHimHungry() {
+        while (true) {
+            if (hBModifier.getSpriteIndex() == 4) {
+                SceneManager.LoadScene("Game");
+            }
+            
+            yield return new WaitForSeconds(hungerTime);
+            hBModifier.ChangeHBar(1);
+        }
+        
+    }*/
+
 }
